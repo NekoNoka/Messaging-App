d7cec635fd8f5bff3d298808a78151a36a89c211
mrbll48 <william.marks87@gmail.com>
2023-06-24 17:20:26 -0700

    worked in homeRoutes.js, will need to be edited as more work is done to app

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+router.get("/", async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    const users = userData.map((data) => data.get({ plain: true }));
+    res.render("homepage", {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+module.exports = router;
