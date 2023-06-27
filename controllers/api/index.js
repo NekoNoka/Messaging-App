ea91dd8d2fc7221e42cbc7ff7f5044d0e76b2e88
mrbll48 <william.marks87@gmail.com>
2023-06-26 19:15:43 -0700

    add connections to server.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
+const whoami = require("./whoAmI");
 
 router.use("/users", userRoutes);
+router.use("/whoami", whoami);
 
 module.exports = router;
